9a54604b20d5236a7abd82d642cc448699f557bb
Rubén Cordero <rubencordero003@gmail.com>
2026-02-26 17:47:18 +0100

    Implement Game Config page

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -3,7 +3,7 @@ export default function Logo() {
         <img
             src="trobar-la-flota-logo.webp"
             alt="Trobar la flota logo"
-            className="relative w-28 md:w-36 drop-shadow-[0_0_20px_rgba(56,189,248,0.4)] hover:scale-105 transition-all duration-300"
+            className="relative w-64 md:w-80 lg:w-96 drop-shadow-[0_0_20px_rgba(56,189,248,0.4)] hover:scale-105 transition-all duration-300"
         />
     )
 }
